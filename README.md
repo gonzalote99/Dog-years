646b8fe2a8faf8b45c30dc5684034a401298d2e4
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-03-27 13:33:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dog-years
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-eym1zh)
---
+++ script.js
@@ -0,0 +1,20 @@
+const myAge = 27;
+
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+console.log(`age dog first 2 years is: ${earlyYears}` );
+
+
+let lateYears = myAge - 2;
+
+lateYears *= 4;
+
+console.log(`years after that: ${lateYears}`);
+
+let myAgeInDogYears = earlyYears + lateYears;
+
+
+const myName = 'Gon To'.toLowerCase();
+console.log(`my name is ${myName}, i am ${myAge}, and ${myAgeInDogYears} in dog years `)
